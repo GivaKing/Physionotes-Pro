0298dcd0f38476c6712458784008523dcc557aa0
GivaKing <ericli@livemail.tw>
2025-12-31 00:04:30 +0800

    Add files via upload

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// 使用您提供的 Key。在真實生產環境中，建議使用 import.meta.env
+const SUPABASE_URL = "https://yvacvzulhaaehffbhttt.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2YWN2enVsaGFhZWhmZmJodHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4OTg1ODUsImV4cCI6MjA4MTQ3NDU4NX0.6tgcZoxClNCn9y5lft_H1XF2pfQGPb6beehSuQLEMD0";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
